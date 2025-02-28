bc2f08a60d1e41ee5ed9e326e15ac2ad38f0858f
Shek Yu Wong <stephensyw@gmail.com>
2025-02-28 07:21:01 -0500

    navigation added

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
+import { provideRouter } from "@angular/router";
+import { routes } from "./app/app.routes";
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+    providers: [
+        provideRouter(routes)
+    ]
+});
